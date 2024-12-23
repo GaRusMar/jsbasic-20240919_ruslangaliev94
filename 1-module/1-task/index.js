d2dbadf9c30437188dd0f3b58da4febe9dd7dd9e
ruslan <ruslangaliev94@gmail.com>
2024-12-23 11:42:26 +0300

    fix 1 module 4 task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,8 +1,47 @@
 function factorial(n) {
-  let result = 1;
-  for (let i = n; i > 1; i--) {
-    result = result * i;
-  }
-  return result;
+    let result = 1;
+    for (let i = n; i > 1; i--) {
+        result = result * i;
+    }
+    return result;
 }
+
+
+
+// factorial(0)
+// factorial(1)
+// factorial(2)
+// factorial(3)
+// factorial(4)
+// factorial(5)
+// function factorial(n) {
+//     if (n === 0 || n === 1) {
+//         console.log(`${n}! = 1`); // Выводим аккуратный результат для 0! или 1!
+//         return 1; // Возвращаем результат
+//     }
+//     let result = 1;
+//     let arr = [];
+//     for (let i = n; i > 1; i--) {
+//         result *= i
+//         arr.push(i)
+//     }
+//     let arrString =  arr.join(' * ')
+//     console.log(`${n} ! = ${arrString} * 1  = ${result}`)
+//     return result
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,7 +1,20 @@
+// function checkSpam(str) {
+//   if (str.includes("xxx") || str.includes("XXX") || str.includes("1XbeT ") || str.includes("1xbet")) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+
+
 function checkSpam(str) {
-  if (str.includes("xxx") || str.includes("XXX") || str.includes("1XbeT ") || str.includes("1xbet")) {
-    return true;
-  } else {
-    return false;
-  }
+    let checkStr = str.toLowerCase();
+
+    if (checkStr.includes("xxx") || checkStr.includes("1xbet") ) {
+        return true;
+    } else {
+        return false;
+    }
 }
+

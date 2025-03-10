5427db128198b84ca87ec65a29619beb3968abb1
ruslan <ruslangaliev94@gmail.com>
2025-03-10 16:04:16 +0300

    test

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,55 +1,85 @@
+// function highlight(table) {
+//
+//   let list = table.tBodies[0].rows;
+//
+//   for (let row of list) {
+//
+//     let statusCell = null;
+//     let genderCell = null;
+//     let ageCell = null;
+//
+//     for (let cell of row.cells) {
+//
+//
+//
+//       if (cell.dataset.available === 'true') {
+//         statusCell = cell;
+//       } else if (cell.dataset.available === 'false') {
+//         statusCell = cell;
+//       } else {
+//         statusCell = cell;
+//       }
+//
+//       if (cell.innerHTML === 'm') {
+//         genderCell = cell;
+//       }
+//       if (cell.innerHTML === 'f') {
+//         genderCell = cell;
+//       }
+//
+//       if (parseInt(cell.textContent) < 18) {
+//         ageCell = cell;
+//       }
+//
+//       if (statusCell) {
+//         if (statusCell.dataset.available === 'true') {
+//           row.classList.add('available');
+//         } else if (statusCell.dataset.available === 'false') {
+//           row.classList.add('unavailable');
+//         } else {
+//           row.hidden = true;
+//         }
+//       }
+//       if (genderCell) {
+//         if (genderCell.innerHTML === 'm') {
+//           row.classList.add('male');
+//         } else if (genderCell.innerHTML === 'f') {
+//           row.classList.add('female');
+//         }
+//       }
+//       if (ageCell) {
+//         row.style.textDecoration = 'line-through';
+//       }
+//     }
+//   }
+// }
+
+
+
+
 function highlight(table) {
-
   let list = table.tBodies[0].rows;
-
   for (let row of list) {
-
-    let statusCell = null;
-    let genderCell = null;
-    let ageCell = null;
-
     for (let cell of row.cells) {
 
-
+      if (cell.textContent === 'm') {
+        row.classList.add('male')
+      } else if (cell.textContent === 'f') {
+        row.classList.add('female')
+      }
 
       if (cell.dataset.available === 'true') {
-        statusCell = cell;
+        row.classList.add('available')
       } else if (cell.dataset.available === 'false') {
-        statusCell = cell;
-      } else {
-        statusCell = cell;
+        row.classList.add('unavailable')
       }
 
-      if (cell.innerHTML === 'm') {
-        genderCell = cell;
+      if (cell.textContent < 18) {
+        row.style.textDecoration = 'line-through'
       }
-      if (cell.innerHTML === 'f') {
-        genderCell = cell;
-      }
-
-      if (parseInt(cell.textContent) < 18) {
-        ageCell = cell;
-      }
-
-      if (statusCell) {
-        if (statusCell.dataset.available === 'true') {
-          row.classList.add('available');
-        } else if (statusCell.dataset.available === 'false') {
-          row.classList.add('unavailable');
-        } else {
-          row.hidden = true;
-        }
-      }
-      if (genderCell) {
-        if (genderCell.innerHTML === 'm') {
-          row.classList.add('male');
-        } else if (genderCell.innerHTML === 'f') {
-          row.classList.add('female');
-        }
-      }
-      if (ageCell) {
-        row.style.textDecoration = 'line-through';
-      }
+    }
+    if (!row.querySelector('[data-available]')) {
+      row.hidden = true;
     }
   }
 }

0f2fef68225396ac98be7f2ce56a85f1026d62aa
ruslangaliev <ruslangaliev94@gmail.com>
2025-04-22 01:05:36 +0300

    new 03

---
+++ test/03.js
@@ -0,0 +1,18 @@
+// Функции и массивы
+
+// функция - повторять одно и то же действие например вывод сообщения
+
+// несколько видов объявления функции
+
+//function decloration
+
+function helloUser1() {
+  console.log('Hello пользователь 1');
+}
+
+// функцию можно вызывать нескольо раз
+helloUser1();
+helloUser1();
+helloUser1();
+
+// function expression
